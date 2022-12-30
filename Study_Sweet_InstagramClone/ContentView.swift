81890c178f494708ecd531e9b6a6435f6e95822a
Taeyoung Won <hexter1994@gmail.com>
2022-12-30 18:50:19 +0900

    Initial Commit

---
+++ Study_Sweet_InstagramClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Study_Sweet_InstagramClone/Study_Sweet_InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_Sweet_InstagramCloneApp.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Study_Sweet_InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
