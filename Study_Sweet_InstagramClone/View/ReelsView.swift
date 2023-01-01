fe06bb8a6c2a966950ee94cef05c940417bb08b1
Taeyoung Won <hexter1994@gmail.com>
2023-01-01 23:54:37 +0900

    View : Reels View 생성

---
+++ Study_Sweet_InstagramClone/View/ReelsView.swift
@@ -0,0 +1,20 @@
+//
+//  ReelsView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct ReelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReelsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReelsView()
+    }
+}
