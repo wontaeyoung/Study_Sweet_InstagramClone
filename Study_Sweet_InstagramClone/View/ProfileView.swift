bef1308db8acd4ebe75109a8aa603d07688debb4
Taeyoung Won <hexter1994@gmail.com>
2023-01-01 23:54:22 +0900

    View : Profile View 생성

---
+++ Study_Sweet_InstagramClone/View/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
