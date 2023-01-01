d410c6b613cdae2317cc23f34d794e18185ac31d
Taeyoung Won <hexter1994@gmail.com>
2023-01-01 23:54:53 +0900

    View : Shop View 생성

---
+++ Study_Sweet_InstagramClone/View/ShopView.swift
@@ -0,0 +1,20 @@
+//
+//  ShopView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct ShopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShopView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopView()
+    }
+}
