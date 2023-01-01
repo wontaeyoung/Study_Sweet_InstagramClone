d530e5d9e793c947cfdf4628b75ed5eb569893a9
Taeyoung Won <hexter1994@gmail.com>
2023-01-01 23:55:09 +0900

    View : Main TabView 생성

---
+++ Study_Sweet_InstagramClone/View/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                    }
+                SearchView()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                    }
+                
+                ReelsView()
+                    .tabItem {
+                        Image(systemName: "play.rectangle")
+                    }
+                
+                ShopView()
+                    .tabItem {
+                        Image(systemName: "handbag")
+                    }
+                
+                SearchView()
+                    .tabItem {
+                        Image(systemName: "person.circle")
+                    }
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
