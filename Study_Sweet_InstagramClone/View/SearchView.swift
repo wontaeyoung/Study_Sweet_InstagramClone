6a18eb2ac60e306f27b86aca73a096d62e4ec3e2
Taeyoung Won <hexter1994@gmail.com>
2023-01-01 23:54:46 +0900

    View : Search View 생성

---
+++ Study_Sweet_InstagramClone/View/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
