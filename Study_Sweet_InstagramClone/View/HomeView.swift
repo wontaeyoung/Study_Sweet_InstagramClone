a08bcdf71c6ec82a5765503032b3d7c8bb0a4983
Taeyoung Won <hexter1994@gmail.com>
2023-01-01 23:54:09 +0900

    View : Home View 생성

---
+++ Study_Sweet_InstagramClone/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Study_Sweet_InstagramClone
+//
+//  Created by 원태영 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
