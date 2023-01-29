007ca722c4fdb59bbfbef6cdb56404d15284c121
Taeyoung Won <hexter1994@gmail.com>
2023-01-30 00:44:48 +0900

    [Feat] #1 shorts Index 세팅 함수 추가, searchable 속성 추가

--- Study_Sweet_InstagramClone/View/Search/SearchView.swift
+++ Study_Sweet_InstagramClone/View/Search/SearchView.swift
@@ -11,33 +11,49 @@ import SwiftUI
 /// 1. 인스타그램 Grid는 4장 이미지 + 우측 Shorts -> 6장 이미지 -> 좌측 Shorts + 4장 이미지 -> 6장 이미지 순으로 구성되어 있다
 
 struct SearchView: View {
-    let columns : [GridItem] = Array(repeating: .init(.flexible(), spacing: 0), count: 3)
+    
+    @State private var shortsIndexes : [Int] = [3]
+    @State private var searchText : String = ""
+    
+    let columns : [GridItem] = Array(repeating: .init(.flexible(minimum : Utility.imageWidth, maximum: Utility.imageWidth), spacing: 0, alignment: .top), count: 3)
     
     var body: some View {
+        
+        
         ScrollView {
             LazyVGrid(columns: columns, spacing: 0) {
-                
-                ForEach(1..<200) {i in
-                    Color.gray
-                        .border(.black)
+                ForEach(1..<Utility.maximumGridCount, id: \.self) {i in
+                    ImageCell(index: i)
+                        .modifier(OverlayModifier(isShorts: shortsIndexes.contains(i)))
                 }
+            }
+        }
+        .searchable(text: $searchText)
+        
+        .onAppear {
+            setShortsIndexes()
+        }
+    }
+    
+    private func setShortsIndexes() {
+        var appendNumber : Int = 0
+        while appendNumber < Utility.maximumGridCount {
+            if let last = Array(shortsIndexes.enumerated()).last {
+                if last.offset % 2 == 0 {
+                    appendNumber = last.element + 10
+                } else {
+                    appendNumber = last.element + 14
+                }
+                if appendNumber > 200 {
+                    break
+                }
+                shortsIndexes.append(appendNumber)
                 
             }
         }
     }
 }
 
-struct ImageCell : View {
-    
-    let index : Int
-    
-    var body: some View {
-        Color.gray
-            .overlay {
-                Text("\(index)")
-            }
-    }
-}
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
